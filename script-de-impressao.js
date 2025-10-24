e9ea757d64ba1a3b2a4b3063e02b1ad0a12dff00
vitorfeltrin <vitor.feltrin@portoseguro.com.br>
2025-10-24 20:18:50 -0300

    tentando adicionar o firebase

--- script-de-impressao.js
+++ script-de-impressao.js
@@ -1,23 +1,26 @@
-   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-    import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyA06N8YExeX7Vt7y2yaIPV3DgOi62hhBEQ",
-  authDomain: "love-found-me.firebaseapp.com",
-  projectId: "love-found-me",
-  storageBucket: "love-found-me.firebasestorage.app",
-  messagingSenderId: "196735722095",
-  appId: "1:196735722095:web:4897eb31a71ca3b609a39a",
-  measurementId: "G-HQEWTMDDMP"
-};
 
 
-   const app = initializeApp(firebaseConfig);
-    const db = getFirestore(app);
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-  window.escrever = async function() {
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBXQT-maFl-WajyLXE0JcY5XItkfwSKH1c",
+    authDomain: "love-found-me-a6c2a.firebaseapp.com",
+    projectId: "love-found-me-a6c2a",
+    storageBucket: "love-found-me-a6c2a.firebasestorage.app",
+    messagingSenderId: "312759140695",
+    appId: "1:312759140695:web:601495647fb1df98c27e12"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  let escrever = async () => {
         
-        let tema = document.getElementById('tema').value;
+        let tema = document.getElemenyId('tema').value;
  
         let conteudo = document.getElementById('conteudo').value;
  
@@ -26,7 +29,6 @@ const firebaseConfig = {
           await setDoc(doc(db, "pregacoes", "ultima_palavra"), {
             tema: tema,
             conteudo: conteudo,
-            atualizadoEm: new Date().toISOString()
           });
           location.href = 'Palavra.html';
           alert("Palavra adicionada com sucesso !");
